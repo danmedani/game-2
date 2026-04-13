41da7fdbf7f2b7acb95bbb17883f696bcfc5a2f0
dan medani <danmedani@gmail.com>
2026-04-12 17:06:53 -0700

    new score url for moon game

--- moon-game/js/config.js
+++ moon-game/js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  scoresUrl: 'https://script.google.com/macros/s/AKfycbzUDs88NQvP6YV-JuhLmQpEhJsQVSGjUgDbkb-8PgqR7emtW0pXFHULkVCG5VZw__aN/exec',
+    scoresUrl: 'https://script.google.com/macros/s/AKfycbywalth1miT6UL926y2Jcx_QoSYg73TPsEf4p6NXeP3gNG8CALjvymg_Fa-ajGT6GoF/exec',
   game: 'MoonGame',
 };
